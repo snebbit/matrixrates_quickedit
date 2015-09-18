015deff024dd1e081e39a7a1814298c5db3a423c
snebbit <tom@tomcalpin.co.uk>
2015-09-18 12:15:25 +0100

    Added floatThead

--- delivery.php
+++ delivery.php
@@ -31,17 +31,19 @@ if(Mage::getSingleton('admin/session')->isLoggedIn()){
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/floatthead/1.2.13/jquery.floatThead.min.js"></script>
         <style>
             *{ font-family: arial,tahoma,sans-serif; color: #222; }
             #delivery-table{ min-width: 700px; }
             #delivery-table form{ display:none; }
             #delivery-table input{ font-size: 11px; }
-            #delivery-table tr:first-child{ background: #ddd; }
-            #delivery-table tr:not(:first-child):hover{ background: #f3f3f3; }
-            #delivery-table tr td:last-child{ width: 33%; text-align: center; }
-            #delivery-table td, #delivery-table th{ padding: 8px 12px; }
+            #delivery-table thead tr th,.floatThead-table thead tr th{ background: #ddd; }
+            #delivery-table tbody tr:hover td{ background: #f3f3f3; }
+            #delivery-table tr td:last-child{ width: 33%; text-align: center; font-size: 20px; }
+            td,th{ padding: 8px 12px; }
+            #delivery-table td:first-child{ font-size: 28px; }
             #delivery-table span.price-editable:hover{ cursor: pointer; }
             #delivery-table span.price-editable icon{ visibility: hidden; display: inline-table; width: 16px; height: 16px; background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA90lEQVQ4T5XTPytHURzH8dfvGVhQymAw4REopZ/FogzKQHkMymI2eSCegFIGrCb8kj8Dk4WBxSilb51Tp8O97r11p3Pf78/nfM+5A/2eeexjF6+BDnrwc7jAOB6wjLeugllM4hhjKfQ+JF0ECzjDFQ4qydF/gtjzeaodwaeF5BvDNkEN53FlySdGTYImOCQfWMRj0ymU064PKeAVXOeFukEvuG4QcAxs4o+7EclD3NRrucEMLhvg91T7F1w2WMMO1quEVjgLpvGMLWwWkoCj9qjtuscWVnGCr0Ky1AXODfZwmFJCsoE7PHX50aLBNqZwm96XLmD+5gfAsjP7wQAa3gAAAABJRU5ErkJggg==);}
             #delivery-table span.price-editable:hover icon{ visibility: visible;}
@@ -49,13 +51,15 @@ if(Mage::getSingleton('admin/session')->isLoggedIn()){
     </head>
     <body>
         <table id="delivery-table">
-            <tr>
-                <th>Postcode</th>
-                <th>Cart Total</th>
-                <th>Weight</th>
-                <th>Shipping Price</th>
-            </tr>
-
+            <thead>
+                <tr>
+                    <th>Postcode</th>
+                    <th>Cart Total</th>
+                    <th>Cart Weight</th>
+                    <th>Shipping Price</th>
+                </tr>
+            </thead>
+            <tbody>
             <?php foreach($shippingRates as $rate){?>
                 <tr id="row-<?php echo $rate['pk'];?>">
                     <td><?php echo $rate['dest_zip'].(strlen($rate['dest_zip'])<1 ? '<i>['.$rate['dest_country_id'].'] *</i>' : '');?></td>
@@ -72,10 +76,12 @@ if(Mage::getSingleton('admin/session')->isLoggedIn()){
                     </td>
                 </tr>
             <?php }?>
-
+            </tbody>
         </table>
         <script>
             $(document).ready(function(){
+                var $table = $('#delivery-table');
+                $table.floatThead();
                 $('span.price-editable').each(function(){
                     $(this).click(function(){
                         $(this).hide();
